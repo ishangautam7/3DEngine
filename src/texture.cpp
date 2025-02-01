68d0206a6d6d910d53651c4db8e0284703216ba3
KUSHAL K.C. <84504998+sakul7-stack@users.noreply.github.com>
2025-02-01 22:13:39 +0545

    Update texture.cpp

--- src/texture.cpp
+++ src/texture.cpp
@@ -3,39 +3,32 @@
 #include "structures.h"
 #include <glad/glad.h>
 #include <iostream>
+#include <string>
 
 unsigned int TextureFromFile(const char* path, const std::string& directory) {
     std::string filename = directory + '/' + path;
-    
-    // Debug print full texture path
     std::cout << "Attempting to load texture: " << filename << std::endl;
 
     unsigned int textureID;
     glGenTextures(1, &textureID);
-
+    
     int width, height, nrComponents;
     unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
     
     if (data) {
-        GLenum format = GL_RGB;
-        if (nrComponents == 1)
-            format = GL_RED;
-        else if (nrComponents == 3)
-            format = GL_RGB;
-        else if (nrComponents == 4)
-            format = GL_RGBA;
-
+        GLenum format = (nrComponents == 1) ? GL_RED :
+                        (nrComponents == 3) ? GL_RGB : GL_RGBA;
+                        
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-
+        
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        stbi_image_free(data);
         
+        stbi_image_free(data);
         std::cout << "Texture loaded successfully: " << filename << std::endl;
     } else {
         std::cerr << "Failed to load texture: " << filename << std::endl;
