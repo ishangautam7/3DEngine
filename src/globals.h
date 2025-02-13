d32f8eba5e9d0bab2ff8d78905c05f163c8c80f6
KUSHAL K.C. <84504998+sakul7-stack@users.noreply.github.com>
2025-02-13 10:06:44 +0545

    Update globals.h

--- src/globals.h
+++ src/globals.h
@@ -1,13 +1,19 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
+#include <string> 
 
 #include <glm/glm.hpp>
+ // Add this line
+
 
 extern bool usePerspective;
 extern float orthoSize;
 extern glm::vec3 modelPosition;
 extern glm::vec3 modelRotation;
+extern glm::vec3 backgroundColor;
 extern float modelScale;
 extern bool wireframeMode;
+extern std::string modelPath;
+
 
 #endif // GLOBALS_H
