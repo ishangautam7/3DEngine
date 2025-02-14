519f57d8578b4c6edad9935a2304aa8380dfc134
KUSHAL K.C. <84504998+sakul7-stack@users.noreply.github.com>
2025-02-14 06:26:31 +0545

    Update globals.cpp
    flip x,y

--- src/globals.cpp
+++ src/globals.cpp
@@ -6,5 +6,7 @@ glm::vec3 backgroundColor(0.2f, 0.3f, 0.3f);
 glm::vec3 modelRotation(0.0f);
 float modelScale = 1.0f;
 bool wireframeMode = false;
+bool flipX = false;  
+bool flipY = false;
 std::string modelPath = "D:/3DEngine/assests/infernus.glb"; // Initial path
 
